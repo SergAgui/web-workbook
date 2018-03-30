a75ac193ba8fd4ce7a1133c28c705d0221dfa517
Sergio Aguilar <sergeagui@gmail.com>
2018-03-29 20:16:04 -0500

    final

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,55 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // You code here
-});
+window.onload = function() {
+// Counting the number of lists.
+    var list = document.getElementsByTagName('ul');
+    var message = "This page has " + list[0].children.length + " list items!";
+    alert(message);
+
+// Adding an h2 under the "<h1> Shopping Cart".
+    var newh2 = document.createElement('h2');
+    newh2.innerHTML = "You have " + list[0].children.length + " items in your shopping cart";
+    document.getElementsByTagName('h1')[0].appendChild(newh2);
+
+  // Adding items to the list
+    var newLi = document.createElement('li');
+
+    var text = document.createElement('input');
+    text.setAttribute('id', 'input');
+    text.setAttribute("type", "text");
+    text.setAttribute("value", " ");
+    list[0].appendChild(text);
+
+    var btn = document.createElement('button');
+    btn.setAttribute('id', 'button');
+    var btnText = document.createTextNode("Add");
+    btn.appendChild(btnText);
+    list[0].appendChild(btn);
+
+    var rmvBtn = document.createElement('button');
+    rmvBtn.setAttribute('id', 'remove');
+    var rmvTxt = document.createTextNode("Remove");
+    rmvBtn.appendChild(rmvTxt);
+    list[0].appendChild(rmvBtn);
+
+
+    document.getElementById("button").addEventListener("click", addItem);
+
+    document.getElementById("remove").addEventListener("click", rmvItem);
+
+    function addItem() {
+      newLi.setAttribute('id', input.value);
+      newLi.appendChild(document.createTextNode(input.value));
+      list[0].prepend(newLi);
+    }
+
+    function rmvItem() {
+      var item =  document.getElementById(input.value);
+      list[0].removeChild(item);
+    }
+};
+
+// 1. Alert the user to how many list items there are on the page. !!! \m/ !!!
+// 2. Add a <h2> to the page under the title that says 'You have _ items in your shopping cart'. Do not use html to do this. Use JavaScript. \m/
+// 3. Create the ability to add more items to the list using JavaScript.
+// 4. Create the ability to remove an item from the shopping cart using javascript.
--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,37 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  $('.draggable').draggable({
+    revert: "invalid"
+  });
+  $('#dropArea1, #dropArea2, #dropArea3').droppable({
+    drop: function(event, ui){
+      //drag is to get the value of the current block that is moved
+      var drag = $(ui.draggable).data('block');
+
+      //last is to get the value of the last block that was placed
+      var last = $(this).children().last().data('block');
+
+      //if the drag'd block's value is larger than the last block in the row, then the move is illegal
+      if (drag > last){
+        $(ui.draggable).draggable( "option", "revert", true );
+      }else{
+        $(ui.draggable).appendTo($(this)).attr('style', 'position: relative');
+      };
+
+      checkWin1();
+
+      // $(ui.draggable).appendTo($(this)).attr('style', 'position: relative');
+    }
+  });
 });
+
+function checkWin1(){
+  if($('[data-stack=3]').children().length == 4){
+    $('#announce-game-won').text('Winner!')
+  };
+};
+
+// $('#clear').click(function) {
+//
+// };
